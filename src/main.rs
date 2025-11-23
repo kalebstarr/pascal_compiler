0cce905fbea30bc789e15d97a8bf2d505340b911
Kaleb <kaleb.starr@proton.me>
2025-11-23 17:33:41 +0100

    Add argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::{env, error::Error, fmt::Display, process};
+
 use lalrpop_util::lalrpop_mod;
 
 lalrpop_mod!(
@@ -9,7 +11,37 @@ lalrpop_mod!(
 pub mod ast;
 
 fn main() {
-    // println!("{}", grammar::TermParser::new().parse("22").unwrap());
-    let out = format!("{:?}", grammar::ExprParser::new().parse("(10 + 22) * (12 / 70)").unwrap());
-    println!("{out}");
+    let file_path = read_args().unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        usage_tip();
+        process::exit(1);
+    });
+
+    println!("{file_path}");
+}
+
+#[derive(Debug)]
+struct ArgsError {
+    msg: String,
+}
+
+impl Error for ArgsError {}
+impl Display for ArgsError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.msg)
+    }
+}
+
+fn read_args() -> Result<String, ArgsError> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        return Err(ArgsError { msg: "Too few arguments".to_string() })
+    }
+
+    Ok(args[1].clone())
+}
+
+fn usage_tip() {
+    println!("Usage: cargo run -- <Filename.pas>");
 }
