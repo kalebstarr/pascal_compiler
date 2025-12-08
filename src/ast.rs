83a1d45a2184c13913a2a5e036c1db5e1ae69896
Kaleb <kaleb.starr@proton.me>
2025-12-08 20:25:31 +0100

    Reorder ast
    Forgot to do this while reordering the grammar

--- src/ast.rs
+++ src/ast.rs
@@ -9,48 +9,6 @@ pub enum Header {
 }
 
 #[derive(Debug, Clone, PartialEq)]
-pub enum Literal {
-    Integer(i32),
-    Double(f64),
-    String(String),
-    Boolean(bool),
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub enum Expr {
-    Literal(Literal),
-    Identifier(String),
-    Binary(Box<Expr>, BinaryOp, Box<Expr>),
-    Unary(UnaryOp, Box<Expr>),
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub enum BinaryOp {
-    Mul,
-    Div,
-    Mod,
-    Add,
-    Sub,
-
-    Eq,
-    Lt,
-    Gt,
-    Le,
-    Ge,
-    Neq,
-
-    And,
-    Or,
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub enum UnaryOp {
-    Not,
-    Pos,
-    Neg,
-}
-
-#[derive(Debug, Clone, PartialEq)]
 pub struct VariableDeclaration {
     pub identifier: String,
     pub typ: Type,
@@ -90,4 +48,46 @@ pub struct IfElse {
 pub struct While {
     pub expr: Box<Expr>,
     pub statement: Box<Statement>,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Literal {
+    Integer(i32),
+    Double(f64),
+    String(String),
+    Boolean(bool),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Expr {
+    Literal(Literal),
+    Identifier(String),
+    Binary(Box<Expr>, BinaryOp, Box<Expr>),
+    Unary(UnaryOp, Box<Expr>),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum BinaryOp {
+    Mul,
+    Div,
+    Mod,
+    Add,
+    Sub,
+
+    Eq,
+    Lt,
+    Gt,
+    Le,
+    Ge,
+    Neq,
+
+    And,
+    Or,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum UnaryOp {
+    Not,
+    Pos,
+    Neg,
 }
